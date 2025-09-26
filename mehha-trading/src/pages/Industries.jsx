ca506d090cbde3a22cbfaaedc70610505077ae54
Prodaaron <aaronmesfin60@gmail.com>
2025-09-26 14:33:23 +0300

    Major change on industries page. (Now general company page whith shares)

--- mehha-trading/src/pages/Industries.jsx
+++ mehha-trading/src/pages/Industries.jsx
@@ -1,14 +1,85 @@
-import React from 'react';
+import React, { useState } from "react";
 import UnderConstruction from '../components/UnderConstruction';
+import "./industry.css";
+import { companies } from '../services/data.js';
+
+const categories = ["Manufacturing", "Import & Export", "Real estate", "Education"];
 
 const Industries = () => {
+  const [selectedCategories, setSelectedCategories] = useState([]);
+
+  const handleCategoryChange = (category) => {
+    if (category === "All") {
+      if (selectedCategories.length === categories.length) {
+        setSelectedCategories([]);
+      } else {
+        setSelectedCategories([...categories]);
+      }
+    } else {
+      setSelectedCategories((prev) =>
+        prev.includes(category)
+          ? prev.filter((c) => c !== category)
+          : [...prev, category]
+      );
+    }
+  };
+
+  const isAllSelected = selectedCategories.length === categories.length;
+
+  const filteredCompanies =
+    selectedCategories.length === 0
+      ? companies
+      : companies.filter((company) =>
+          selectedCategories.includes(company.category)
+        );
+
   return (
-    <div className='industries'>
-      {/* <UnderConstruction /> */}
-
+    <div className="industry">
       <h1>Our Companies & Shares</h1>
-    </div>
-  )
-}
+      <hr />
 
-export default Industries
+      <p className="Industry-label">Industry</p>
+      {/* Categories filter */}
+      <div className="category-filters">
+        <label className="filter-option">
+          <input
+            type="checkbox"
+            checked={isAllSelected}
+            onChange={() => handleCategoryChange("All")}
+            ch
+          />
+          All
+        </label>
+
+        {categories.map((cat) => (
+          <label key={cat} className="filter-option">
+            <input
+              type="checkbox"
+              checked={selectedCategories.includes(cat)}
+              onChange={() => handleCategoryChange(cat)}
+            />
+            {cat}
+          </label>
+        ))}
+      </div>
+
+      {/* Companies list */}
+      <div className="company-list">
+        {filteredCompanies.map((company) => (
+          <a
+            key={company.id}
+            href={company.link}
+            className="company-card"
+          >
+            <img className="list-logo" src={company.logo} alt={company.name} />
+            <h2>{company.name}</h2>
+            <p>{company.category}</p>
+          </a>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+export default Industries;
--- mehha-trading/src/services/data.js
+++ mehha-trading/src/services/data.js
@@ -1,30 +1,50 @@
 export const companies = [
     {
+        id: 'comp-1-07-25-25',
+        name: 'MEHHA Import & Export',
+        logo: './Main.png',
+        category: 'Import & Export',
+        description: '',
+        link: ''
+    },
+    {
         //          mm-dd-yy
         id: 'comp-1-08-17-25',
-        name: 'Mes-Arg Plastich Shoe & Raw Material',
-        logo: './Main.png',
+        name: 'Mes-Arg Plast',
+        logo: './mes-arg-plast.png',
         category: 'Manufacturing',
-        description: ''
+        description: '',
+        link: ''
     },
 
     {
         //          mm-dd-yy
         id: 'comp-2-08-17-25',
         name: 'Tirsit Apartment',
-        logo: './Tirsit.png',
+        logo: './tirsit-apartment-white.png',
         category: 'Real estate',
-        description: ''
+        description: '',
+        link: ''
     },
 
+    // {
+    //     //          mm-dd-yy
+    //     id: 'comp-3-08-17-25',
+    //     name: 'Yegemya Trading PLC',
+    //     logo: './Main.png',
+    //     category: 'Real estate',
+    //     description: '',
+    //     link: ''
+    // },
+
     {
-        //          mm-dd-yy
-        id: 'comp-3-08-17-25',
-        name: 'Yegemya Trading PLC',
-        logo: './Main.png',
-        category: 'Real estate',
-        description: ''
-    },
+        id: 'comp-2-09-25-25',
+        name: 'Zega International School',
+        logo: './zega.png',
+        category: 'Education',
+        description: '',
+        link: 'https://zegainternational.school/'
+    }
 ]
 
 export const exportProducts = [
