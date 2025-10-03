351565cdc7c942580c4f746f09ceaaf04975cd14
Prodaaron <aaronmesfin60@gmail.com>
2025-10-03 16:28:19 +0300

    Minor changes to all "mes-arg plastic shoe and raw material" to "mes-arg plast"

--- mehha-trading/src/components/meshero.jsx
+++ mehha-trading/src/components/meshero.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './meshero.css';
 
+
 import meshero from '../assets/pvc-bg.png';
 
 const mesHeroSection = () => {
   return (
     <div className='meshero' style={{backgroundImage: `url(${meshero})`}}>
       <div className="meshero-content">
-        <h1>MES-ARG Plastic Shoe and Raw Material <sub>&reg;</sub></h1>
+        <img className='hero-logo' src="./mes-arg-plast.png" alt="" />
 
         <h2>More Than Shoes. More Than Materials. <br /> A Legacy of Quality.</h2>
         <p>Proudly serving Ethiopia for 15 years with locally made PVC shoes and raw materials.</p>
--- mehha-trading/src/components/pvcRawSection.jsx
+++ mehha-trading/src/components/pvcRawSection.jsx
@@ -6,7 +6,7 @@ const pvcRawSection = () => {
   return (
     <div className='pvc-section' style={{backgroundImage: `url(${rawMaterial})`, backgroundSize: 'cover', backgroundPosition: 'center'}}>
       <div className="content-card">
-        <p>For over 5 years, Mes-Arg Plastic Shoe and Raw Material has been at the forefront of Ethiopia’s plastic manufacturing sector, producing PVC raw materials locally rather than relying on imports. This strategic shift has allowed us to play a vital role in supporting the country’s industrial growth while strengthening our own production capacity.</p>
+        <p>For over 5 years, Mes-Arg Plast has been at the forefront of Ethiopia’s plastic manufacturing sector, producing PVC raw materials locally rather than relying on imports. This strategic shift has allowed us to play a vital role in supporting the country’s industrial growth while strengthening our own production capacity.</p>
 
         <p>
             Throughout these years, we have remained resilient and reliable, consistently supplying high-quality PVC raw materials to various industries that depend on them for their manufacturing processes. At the same time, we have successfully integrated these raw materials into our own production line, enabling us to manufacture durable and in-demand PVC shoes, boots, and flops.
