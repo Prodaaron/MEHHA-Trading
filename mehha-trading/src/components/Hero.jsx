6510be9ba16aa12641805193f3f23ccc22ff56e0
prodaaron <aaronmesfin60@gmail.com>
2025-11-22 22:58:38 +0800

    New layout - mehha hero section redesign

--- mehha-trading/src/components/Hero.jsx
+++ mehha-trading/src/components/Hero.jsx
@@ -1,21 +1,30 @@
 import React from "react";
 import "./Hero.css";
-import heroBg from "../assets/Unity-bg.png"; // Replace with your actual background image
+import heroBg from "../assets/hero-image-2025-26-enterance.png"; // Replace with your actual background image
 import logo from "../assets/logo.png"; // Your MEHHA logo
 
 const Hero = () => {
   return (
     <section
       className="hero"
-      style={{ backgroundImage: `url(${heroBg})` }}
+      
     >
-      <div className="hero-overlay">
-        <img src={logo} alt="MEHHA Trading PLC" className="hero-logo" />
-        <h1>Unity in Every Step.</h1>
-        <p>Working to make global companies part of our family.</p>
-        <a href="/exports">
-          <button className="hero-btn">Learn More</button>
-        </a>
+      <div className="hero-left">
+        {/* <img src={logo} alt="MEHHA Trading PLC" className="hero-logo" /> */}
+        <div className="hero-left-content">
+          <h1>Unity in Every Step.</h1>
+          <p>Working to make companies worldwide part of our family.</p>
+          <a href="/exports">
+            <button className="hero-btn">Learn More</button>
+          </a>
+        </div>
+      </div>
+
+      <div 
+        className="hero-right"
+        style={{ backgroundImage: `url(${heroBg})` }}
+        >
+
       </div>
     </section>
   );
