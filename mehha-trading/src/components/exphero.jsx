bef670c54f734c43cd4fb93ade3a00101b4e3b3d
prodaaron <aaronmesfin60@gmail.com>
2025-12-04 17:53:09 +0800

    New-layout exphero section update

--- mehha-trading/src/components/exphero.jsx
+++ mehha-trading/src/components/exphero.jsx
@@ -1,21 +1,20 @@
 import React from "react";
 import "./expHero.css";
-import heroBg from "../assets/ethiopia-map.jpg"; // Replace with your actual background image
+import heroBg from "../assets/hero-image-2025-26-enterance-export-page.png"; // Replace with your actual background image
 import logo from "../assets/logo.png"; // Your MEHHA logo
 
 const expHero = () => {
   return (
     <section
       className="exphero"
-      style={{ backgroundImage: `url(${heroBg})` }}
     >
       <div className="exphero-overlay">
-        <img src={logo} alt="MEHHA Trading PLC" className="hero-logo" />
-        <h1>Uniting to Make the World a Better Place.</h1>
-        <p>Building on our initial success, we now plan to expand into the export
-        sector, focusing on oilseeds and pulses. Our company aims to supply high-quality products and become one of the best supplier and contribute to the global market. </p>
-        
+        {/* <img src={logo} alt="MEHHA Trading PLC" className="hero-logo" /> */}
+        <h1>Humera Gonder Sesame.</h1>
+        <p>Better Tahini. Better Oil. Better Life.</p>
       </div>
+
+      <img src={heroBg} alt="" className="exp-hero-img" />
     </section>
   );
 };
