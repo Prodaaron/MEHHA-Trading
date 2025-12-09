2cebafe6f9ed29fe8e3da12932bd66f50d15c74d
prodaaron <aaronmesfin60@gmail.com>
2025-12-10 00:59:49 +0800

    commenting out unused imports

--- mehha-trading/src/pages/Home.jsx
+++ mehha-trading/src/pages/Home.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 import "./home.css";
 import HeroSection from "../components/Hero.jsx"; // capitalized component
-import List from '../components/list.jsx';
-import VisionSection from '../components/VisionSection.jsx'
-import MissionSection from "../components/MissionSection.jsx";
-import ShuffleSection from "../components/ShuffleSection.jsx";
-import ListingSec from '../components/listing-sec.jsx';
+// import List from '../components/list.jsx';
+// import VisionSection from '../components/VisionSection.jsx'
+// import MissionSection from "../components/MissionSection.jsx";
+// import ShuffleSection from "../components/ShuffleSection.jsx";
+// import ListingSec from '../components/listing-sec.jsx';
 import TextSlider from "../components/TextSlider.jsx";
 import SecondSection from "../components/secondSection.jsx";
 import WhyChooseMehha from "../components/WhyChooseMehha.jsx";
-import ProductsSection from "../components/ProductsSection.jsx";
+// import ProductsSection from "../components/ProductsSection.jsx";
 import ExportInfo from "../components/ExportInfo.jsx";
 import PartnershipForm from "../components/PartnershipForm.jsx";
 
